a1db1843df162076257d6531b54baab4cd4e18cc
Bellasacc <bellasacc10@gmail.com>
2023-03-09 12:40:20 -0500

    componente navbar

---
+++ src/components/navBar/Navbar.js
@@ -0,0 +1,12 @@
+import "./Navbar.css"
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <nav className='navbar'>
+        <h1 className='navbar-logo'>Shop.</h1>
+        <h1 className='seeCarrito'>🛒</h1>
+      </nav>
+    </div>
+  )
+}
+export default Navbar;
